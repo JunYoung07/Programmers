e775cb66e948626328b6625b52b2709a05065230
JunYoung07 <julijylee@gmail.com>
2022-10-08 01:52:05 +0900

    first commit

---
+++ Data_Structure/doublylinkedlist.py
@@ -0,0 +1,106 @@
+class Node:
+    def __init__(self, item):
+        self.data = item
+        self.prev = None
+        self.next = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.nodeCount = 0
+        self.head = Node(None)
+        self.tail - Node(None)
+        self.head.prev = None
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.tail.next = None
+
+    def traverse(self):
+        result = []
+        curr = self.head
+        # self.tail is dummy node
+        while curr.next.next:
+            curr = curr.next
+            result.append(curr.data)
+        return result
+
+    # doubly linked list 이기 때문에 가능
+    def reverse(self):
+        result = []
+        curr = self.tail
+        # self.head is dummy node
+        while curr.prev.prev:
+            curr = curr.prev
+            result.append(curr.data)
+        return result
+
+    def getAt(self, pos):
+        if pos < 0 and pos > self.nodeCount:
+            return None
+        if pos > self.nodeCount // 2:
+            i = 0
+            curr = self.tail
+            while i < self.nodeCount - pos + 1:
+                curr = curr.prev
+                i += 1
+        else:
+            i = 0
+            curr = self.head
+            while i < pos:
+                curr = curr.next
+                i += 1
+        return curr
+
+    def insertAfter(self, prev, newNode):
+        next = prev.next
+        newNode.prev = prev
+        newNode.next = next
+        prev.next = newNode
+        next.prev = newNode
+        self.nodeCount += 1
+        return True
+
+    def insertBefore(self, next, newNode):
+        prev = next.prev
+        newNode.prev = prev
+        newNode.next = next
+        next.prev = newNode
+        prev.next = newNode
+        self.nodeCount += 1
+        return True
+
+    def insertAt(self, pos, newNode):
+        if pos < 1 or pos > self.nodeCount + 1:
+            return False
+        prev = self.getAt(pos - 1)
+        return self.insertAfter(prev, newNode)
+
+    def popAfter(self, prev):
+        curr = prev.next
+        next = curr.next
+        prev.next = next
+        next.prev = prev
+        self.nodeCount -= 1
+        return curr.data
+
+    def popBefore(self, next):
+        curr = next.prev
+        prev = curr.prev
+        next.prev = prev
+        prev.next = next
+        self.nodeCount -= 1
+        return curr.data
+
+    def popAt(self, pos):
+        if pos < 1 or pos > self.nodeCount:
+            return IndexError
+        prev = self.getAt(pos - 1)
+        return self.popAfter(prev)
+
+    def concat(self, L):
+        self.tail.prev.next = L.head.next
+        L.head.next.prev = self.tail.prev
+        self.tail = L.tail
+        self.nodeCount += L.nodeCount
+
+
+
